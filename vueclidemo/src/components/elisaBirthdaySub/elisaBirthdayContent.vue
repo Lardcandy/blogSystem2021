3abd7f50b3b65c6935b24339432141e8afa3371c
root <1053649721@qq.com>
2021-04-13 00:12:41 +0800

    2021-04-13 0012 modify elisaBirthdayContent

--- vueclidemo/src/components/elisaBirthdaySub/elisaBirthdayContent.vue
+++ vueclidemo/src/components/elisaBirthdaySub/elisaBirthdayContent.vue
@@ -1,11 +1,18 @@
 <template>
     <div>
-        <div class="text">
-
+        <div class="openPage" v-if="flowNum === 0">
+            <p>热烈庆祝Elisa 3岁生日！</p>
+            <p>以下是你的忠实拥趸David写给你的一封信</p>
+            <el-button id="open-btn" type="primary" size="small" @click="openPage">打开</el-button>
         </div>
-        <div class="btn">
-            <el-button type='danger' round>去你丫 不接受</el-button>
-            <el-button type='success' round>好啦 接受啦靓仔</el-button>
+        <div class="letterPage" v-else-if="flowNum === 1">
+            <div class="text">
+
+            </div>
+            <div class="btn">
+                <el-button id="cancel-btn" size="mini" round>去你丫 不接受</el-button>
+                <el-button id="submit-btn" type='success' size="mini" round>好啦 接受啦靓仔</el-button>
+            </div>
         </div>
     </div>
 </template>
@@ -15,6 +22,13 @@ export default {
     name: 'app',
     data() {
         return {
+            flowNum: 0,
+        }
+    },
+    methods: {
+        // 0 打开信件
+        openPage() {
+            this.flowNum++
         }
     }
 
@@ -26,14 +40,37 @@ export default {
     margin: 0;
     padding: 0;
 }
+.openPage {
+    float: left;
+    width: 100%;
+    height: 500px;
+    /* background-color: lightgrey; */
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+.openPage>p {
+    font-size: 20px;
+}
+#open-btn {
+    margin: 10px 0 0 0;
+}
 .text {
     width: 100%;
-    height: 400px;
-    background-color: antiquewhite;
+    height: 450px;
+    border-bottom: 1px dotted lightgray;
 }
 .btn {
     width: 100%;
-    height: 100px;
+    height: 50px;
+    display: flex;
+    justify-content: center;
+}
+.btn>.el-button {
+    margin: 10px 0;
+}
+#cancel-btn {
 }
 
 </style>
